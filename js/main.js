d61a1a1aa3aeb8fbe51e133f5d4d2db6414f1401
Wolf <wolf22396@gmail.com>
2020-06-25 18:23:28 +0300

    Сделал таймер по примеру, с фиксом мини багов, таких как отображение при перезагрузке, и подстановка нуля при числе < 10

--- js/main.js
+++ js/main.js
@@ -44,4 +44,74 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Tabs end
+
+    // Timer start
+    // дата окончания
+    let expirationDate = '2020-06-26';
+    differenceTime(expirationDate);
+    // ф-я разницы времени до окончания даты и ненешним
+    function differenceTime(endDate)
+    {
+        // вся разница времени
+        let alldifferenceTime = Date.parse(endDate) - Date.parse(new Date());
+        // часы,минуты,секунды округленные до целого числа
+        let hours = Math.floor((alldifferenceTime / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((alldifferenceTime / (1000 * 60)) % 60),
+            seconds = Math.floor((alldifferenceTime / 1000) % 60);
+
+        // возращаем обьект
+        return {
+            'allTime': alldifferenceTime,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    
+    // ф-я проверки и подстаки нуля
+    function helperZero(num)
+    {
+        if(num >= 0 && num < 10)
+        {
+            return `0${num}`;
+        }
+        else
+        {
+            return num;
+        }
+    }
+
+    // ф-я вывода где селектор это элемент
+    function getTime(selector, endDate)
+    {
+        // получаем родительский элемент
+        let timer = document.querySelector(selector);
+        // получаем его элементы
+        let timerAll = timer.querySelectorAll('span');
+        // вывод счетчика
+        let counter = setInterval(setTime, 1000);
+
+        // запуск ф-и для норм отображения при перезагрузки страницы
+        setTime();
+
+        // выводим на страницу
+        function setTime()
+        {
+            timerAll[0].innerHTML = helperZero(differenceTime(endDate).hours);
+            timerAll[2].innerHTML = helperZero(differenceTime(endDate).minutes);
+            timerAll[4].innerHTML = helperZero(differenceTime(endDate).seconds);
+        }
+
+        //  проверяем если закончилось время
+        if((differenceTime(endDate).allTime) <= 0)
+        {
+            clearInterval(counter);
+        }
+    }
+
+    // запуск  и передача элемента поиска и дата окончания
+    getTime('#timer', expirationDate);
+    // Timer end
 });
